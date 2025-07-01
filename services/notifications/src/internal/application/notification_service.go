6ac4a276591062023bd2f8cbfa6d04ebdf0f5be4
Henrique <henrique.koga@esferaenergia.com.br>
2025-07-01 10:21:19 -0300

    Refactor notification service to use dependency injection and improve email sending logic

--- services/notifications/src/internal/application/notification_service.go
+++ services/notifications/src/internal/application/notification_service.go
@@ -3,17 +3,21 @@ package application
 import (
 	"fmt"
 	"notifications/internal/domain"
-	"notifications/internal/infrastructure/email"
 )
 
-func ProcessNotification(msg domain.CommentMessage) {
+type NotificationService struct {
+	emailSender domain.EmailSender
+}
+
+func NewNotificationService(emailSender domain.EmailSender) *NotificationService {
+	return &NotificationService{
+		emailSender: emailSender,
+	}
+}
+
+func (n *NotificationService) ProcessNotification(msg domain.CommentMessage) error {
 	subject := fmt.Sprintf("Novo comentário de %s", msg.AuthorName)
 	body := fmt.Sprintf("Mensagem: %s\nPúblico: %t", msg.Message, msg.IsPublic)
 
-	err := email.SendEmail(subject, body)
-	if err != nil {
-		fmt.Println("[NOTIFICATION] Erro ao enviar email:", err)
-	} else {
-		fmt.Println("[NOTIFICATION] Notificação enviada com sucesso.")
-	}
+	return n.emailSender.Send(subject, body)
 }
